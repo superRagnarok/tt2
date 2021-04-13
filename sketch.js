ff54c4a570c43beb2d0b24b53e152c107b59c25d
superRagnarok <73694075+superRagnarok@users.noreply.github.com>
2021-04-13 10:04:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,11 +4,11 @@ function preload(){
 introImg = loadImage("intro.png");
 }
 function setup(){
-createCanvas(displayWidth-45,displayHeight-166);
+createCanvas(displayWidth-35,displayHeight-166);
 
 instructions = createSprite(displayWidth/2,displayHeight/2.1,40,40);
 instructions.addImage(introImg);
-instructions.scale=0.5;
+instructions.scale=0.3;
 
 }
 function draw(){
